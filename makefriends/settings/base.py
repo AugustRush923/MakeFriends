1f44dff2746765ae71c437de4f04fd23001dae92
AugustRush <augustrush923@163.com>
2021-09-07 16:02:39 +0800

    面包屑导航

--- makefriends/settings/base.py
+++ makefriends/settings/base.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'blog.middleware.performance.GetPathMiddleware',
     'blog.middleware.performance.PerformanceMiddleware',
     'blog.middleware.user_id.UserIDMiddleware',
     'django.middleware.security.SecurityMiddleware',
